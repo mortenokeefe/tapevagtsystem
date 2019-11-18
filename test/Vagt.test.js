9f0ed90c3e72baa4853fc8e0223ed926069c37e2
Morten O'Keefe <mortenokeefe@gmail.com>
2019-11-18 09:34:56 +0100

    jaja

--- test/Vagt.test.js
+++ test/Vagt.test.js
@@ -1,3 +1,12 @@
-"use strict";
+const should = require('should');
+const {Vagt} = require('../models/Vagt');
 
-Const Vagt = require('./models/Vagt');
+describe('unitTest', () => {
+    it('genererUserTabelundenusers', () => {
+        let vagt = new Vagt('18:00',undefined, undefined, 'Optaget', 'Normal', 'jensb89');
+        (typeof vagt).should.be.equal('object');
+        vagt.bruger.should.be.equal('jensb89');
+    });
+});
+
+//Hvilke andre tests skal der laves??
