0767a0808e3fe8fc8bde4caac529432a079a9048
Morten O'Keefe <mortenokeefe@gmail.com>
2019-11-20 19:15:15 +0100

    jaja

--- models/Vagt.js
+++ models/Vagt.js
@@ -1,16 +1,36 @@
 
-class Vagt {
-    constructor (startTid, fravær, fraværsBeskrivelse, stat, vagtType, bruger) {
-        this.startTid = startTid;
-        this.fravær = fravær;
-        this.fraværsBeskrivelse = fraværsBeskrivelse;
-        this.status = stat;
-        this.vagtType = vagtType;
-        this.bruger = bruger;
-    }
-    toString() {
-        return 'Vagt';
-    }
-}
+// class Vagt {
+//     constructor (startTid, fravær, fraværsBeskrivelse, stat, vagtType, bruger) {
+//         this.startTid = startTid;
+//         this.fravær = fravær;
+//         this.fraværsBeskrivelse = fraværsBeskrivelse;
+//         this.status = stat;
+//         this.vagtType = vagtType;
+//         this.bruger = bruger;
+//     }
+//     toString() {
+//         return 'Vagt';
+//     }
+// }
+//
+// exports.Vagt = Vagt;
 
-exports.Vagt = Vagt;
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const vagt = new Schema({
+    startTid: Date,
+    fravær: Boolean,
+    fraværsBeskrivelse: String,
+    status: Number,
+    vagtType: Number,
+    bruger: Object,
+    begivenhed: Object
+});
+
+vagt.methods.toString = function() {
+    return '';
+};
+
+module.exports = mongoose.model('Vagt', vagt);
--- test/Vagt.test.js
+++ test/Vagt.test.js
@@ -6,6 +6,9 @@ describe('unitTest', () => {
         let vagt = new Vagt('18:00',undefined, undefined, 'Optaget', 'Normal', 'jensb89');
         (typeof vagt).should.be.equal('object');
         vagt.bruger.should.be.equal('jensb89');
+        //sidste del kan jeg ikke få til at fungere og er blevet unladt
+        //let v  = {startTid: '18:00', fravær: undefined, fraværsBeskrivelse: undefined, status: 'Optaget', vagtType: 'Normal', bruger: 'jensb89'};
+        //vagt.should.be.equal(v);
     });
 });
 
