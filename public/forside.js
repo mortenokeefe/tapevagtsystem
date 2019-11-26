09e45eee79a027ee39912863c13ef65c590b1a2b
Morten O'Keefe <mortenokeefe@gmail.com>
2019-11-26 11:18:55 +0100

    jajaj

--- public/forside.js
+++ public/forside.js
@@ -18,6 +18,10 @@ function openPane(evt, cityName) {
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
+
+    if (cityName == 'Frivillige') {
+        getBrugere();
+    }
 }
 
 document.getElementById("defaultOpen").click();
